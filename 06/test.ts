151ecd1ea9f355a40f00f9f94ca62020bfa2956f
mhensley <matt@switcherstudio.com>
2021-12-07 20:50:32 -0500

    day 6

---
+++ 06/test.ts
@@ -0,0 +1,62 @@
+import load from '../load';
+
+function bruteforce(input: string[], days: number) {
+    const init = input[0].split(',').map(x => parseInt(x, 10));
+    let state = init;
+
+    for (const day of Array(days).keys()) {
+        let born = [];
+        state = state.map(fish => {
+            if (fish === 0) {
+                born.push(8);
+                return 6;
+            } else {
+                return fish - 1;
+            }
+        });
+        state = [...state, ...born];
+    }
+
+    return state.length;
+}
+
+function solution(input: string[], days: number) {
+    let buckets = new Array(9).fill(0);
+
+    for (let x of input[0].split(',')) {
+        buckets[x] += 1;
+    }
+
+    for (const day of Array(days).keys()) {
+        const born = buckets.shift();
+        buckets[6] += born;
+        buckets = [...buckets, born];
+    }
+
+    return buckets.reduce((acc, prev) => {
+        return acc + prev;
+    }, 0);
+}
+
+describe('day 6', () => {
+    test('experiment pt1', async () => {
+        const input = await load('./06/sample.txt');
+        expect(solution(input, 18)).toBe(26);
+        expect(solution(input, 80)).toBe(5934);
+    });
+
+    test('puzzle pt1', async () => {
+        const input = await load('./06/input.txt');
+        expect(solution(input, 80)).toBe(365131);
+    });
+
+    test('experiment pt2', async () => {
+        const input = await load('./06/sample.txt');
+        expect(solution(input, 256)).toBe(26984457539);
+    });
+
+    test('puzzle pt1', async () => {
+        const input = await load('./06/input.txt');
+        expect(solution(input, 256)).toBe(1650309278600);
+    });
+});
